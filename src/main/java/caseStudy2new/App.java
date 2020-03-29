8064ed60718fadb51d1d9a902a9c6a651cd8a04d
raghvendra-pratap224 <rahul2011ag@gmail.com>
2020-03-29 21:06:28 +0530

    comments

---
+++ src/main/java/caseStudy2new/App.java
@@ -0,0 +1,13 @@
+package caseStudy2new;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/test_runner/TestRunner.java
@@ -0,0 +1,15 @@
+package test_runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "Feature"
+		,glue= {"test_definition"}
+		)
+public class TestRunner {
+
+}
